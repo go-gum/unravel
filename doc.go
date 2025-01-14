043fbd38a66cb10886283f17bef71d46f86455c2
Oliver Bestmann <oliver.bestmann@googlemail.com>
2025-01-14 08:15:41 +0100

    feat: improve docs

--- doc.go
+++ doc.go
@@ -1,8 +1,13 @@
-// Package serde provides a generic data model to describe access to serialized data. It further
-// provides a [Decoder] type to [Unmarshal] data onto go types (e.g. structs, slices, strings, etc)
-// similar to [json.Unmarshal].
+// Package serde offers a flexible and generic framework for working with serialized data.
+// At its core, it provides tools to map serialized data to native Go types, such as structs,
+// slices, strings, and more. This is achieved using the [Decoder] type, which facilitates
+// the [Unmarshal] process, much like the familiar [encoding/json.Unmarshal].
 //
-// The [SourceValue] defines access to a serialized value. It adapts the underlying representation
-// using a set of functions. The [Decoder.Unmarshal] function walks the target type and pulls data out
-// of the [SourceValue] using functions like [SourceValue.Int], [SourceValue.String], etc.
+// A key component of the package is the [SourceValue], which serves as an abstraction over
+// the underlying serialized data. It provides a suite of methods to extract values in their
+// intended forms, such as [serde.SourceValue.Int] for integers and [serde.SourceValue.String]
+// for strings. During the unmarshaling process, the [Decoder] navigates the structure of the
+// target Go type and retrieves the corresponding data from the [SourceValue]. This design
+// makes it easy to work with a variety of serialized formats while maintaining a consistent
+// API for decoding.
 package serde
